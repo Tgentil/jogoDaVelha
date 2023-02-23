923eff8eb84ab761592f287b015016849a5b7129
Thiago Gentil <thiagosgentil@gmail.com>
2023-02-23 14:49:54 -0300

    comentarios board-components

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -6,40 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board.component.scss']
 })
 export class BoardComponent implements OnInit{
-  quadrados: any[];
-  xIsNext: boolean;
-  vencedor: string;
+  quadrados: any[];  // um array que representa os quadrados do jogo
+  xIsNext: boolean;  // uma variável que controla se é a vez do jogador 'X' ou 'O'
+  vencedor: string;  // uma variável que armazena o vencedor do jogo
 
   constructor() {
   }
 
   ngOnInit() {
-      this.novoJogo();
+      this.novoJogo();  // inicia um novo jogo
   }
 
   novoJogo() {
-    this.quadrados = Array(9).fill(null);
-    this.vencedor = null;
-    this.xIsNext = true;
+    this.quadrados = Array(9).fill(null);  // cria um array com 9 elementos e inicializa com null
+    this.vencedor = null;  // reinicia o vencedor do jogo
+    this.xIsNext = true;   // inicia a vez do jogador 'X'
   }
 
   get jogador() {
-    return this.xIsNext ? 'X' : 'O';
+    return this.xIsNext ? 'X' : 'O';  // retorna o jogador atual
   }
 
   jogada(idx: number) {
 
     if (this.vencedor !== null) {
-      return; // Jogo acabou
+      return; // retorna imediatamente se o jogo já acabou
     }
 
     if (!this.quadrados[idx] ) {
-      this.quadrados.splice(idx, 1, this.jogador);
-      this.xIsNext = !this.xIsNext;
+      this.quadrados.splice(idx, 1, this.jogador);  // atualiza o quadrado clicado com a jogada do jogador atual
+      this.xIsNext = !this.xIsNext;  // muda a vez do jogador
     }
 
-    this.vencedor = this.calcularVencedor();
+    this.vencedor = this.calcularVencedor();  // verifica se houve um vencedor após a jogada
   }
+
   calcularVencedor() {
     const linhas = [
       [0, 1, 2], // linha de cima
@@ -51,6 +52,8 @@ export class BoardComponent implements OnInit{
       [0, 4, 8], // diagonal da esquerda
       [2, 4, 6] // diagonal da direita
     ];
+
+    // verifica se alguma das linhas, colunas ou diagonais tem todas as jogadas iguais
     for (let i = 0; i < linhas.length; i++) {
       const [a, b, c] = linhas[i];
       if (
@@ -58,9 +61,10 @@ export class BoardComponent implements OnInit{
         this.quadrados [a] === this.quadrados[b] &&
         this.quadrados[a] === this.quadrados[c]
       ) {
-        return this.quadrados[a];
+        return this.quadrados[a];  // retorna o vencedor
       }
     }
-    return null;
+
+    return null;  // não houve vencedor ainda
   }
 }
