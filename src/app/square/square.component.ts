e55376d78b0053748487ab7d817c42b32374ecf3
Thiago Gentil <thiagosgentil@gmail.com>
2023-02-23 17:06:37 -0300

    comments

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,12 +3,13 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-      <button>{{ valor }}</button>
+      <button>{{ valor }}</button>  <!-- Define a marcação HTML para o componente -->
   `,
   styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
 })
 export class SquareComponent {
 
-  @Input() valor: 'X' | 'O';
+  @Input() valor: 'X' | 'O'; // Define a entrada de dados como 'valor'
+
 
 }
